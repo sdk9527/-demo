b3ad4df79a1e9becaf9cd392439d5bcdc17571cf
shiyc <1142797508@qq.com>
2023-01-30 17:52:41 +0800

    初次提交

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="getData">获取数据</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  setup() {
+    const getData = async () => {
+      const data = await axios.get(
+        "http://localhost:3022/comment/music?id=186016&limit=1"
+      );
+      console.log(data);
+    };
+
+    return { getData };
+  },
+};
+</script>
+
+<style>
+</style>
